64a6c4e4f85d87793913e1268f9b9542b6671a60
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-06-23 18:06:02 -0700

    AnimalFarmFinal

--- app.js
+++ app.js
@@ -7,3 +7,25 @@
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
+
+  
+const catSound = document.getElementById('cat-sound');
+const cat = document.getElementById('cat');
+cat.addEventListener('click', ()=>{
+    catSound.play();
+})
+const dogSound = document.getElementById('dog-sound');
+const dog = document.getElementById('dog');
+dog.addEventListener('click', ()=>{
+  dogSound.play();
+})
+const horseSound = document.getElementById('horse-sound');
+const horse = document.getElementById('horse');
+horse.addEventListener('click', ()=>{
+  horseSound.play();
+})
+const velociraptorSound = document.getElementById('velociraptor-sound');
+const velociraptor = document.getElementById('velociraptor');
+velociraptor.addEventListener('click', ()=>{
+  velociraptorSound.play();
+})
